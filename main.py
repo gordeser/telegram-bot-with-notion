97d1000d2017a64bf6c931a4d41ef0e80c48b814
Sergei Gordeev <gordeser@cvut.cz>
2023-02-13 22:12:29 +0100

    Add balance command

--- main.py
+++ main.py
@@ -18,6 +18,31 @@ async def send_welcome(message: types.Message):
     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
+
+@dp.message_handler(commands=['balance'])
+async def show_balance(message: types.Message):
+    CZK = 120.004343
+    RUB = 456.0054354
+    EUR = 789.32654654
+    USD = 532.55654654
+
+    # balances = {
+    #     'CZK': CZK,
+    #     'RUB': RUB,
+    #     'EUR': EUR,
+    #     'USD': USD
+    # }
+
+    to_send = f"""
+Your balances
+<b>CZK</b>: <i>{CZK:.2f}</i>
+<b>RUB</b>: <i>{RUB:.2f}</i>
+<b>EUR</b>: <i>{EUR:.2f}</i>
+<b>USD</b>: <i>{USD:.2f}</i>"""
+
+    await message.reply(to_send, parse_mode='html')
+
+
 @dp.message_handler()
 async def echo(message: types.Message):
     # old style:
