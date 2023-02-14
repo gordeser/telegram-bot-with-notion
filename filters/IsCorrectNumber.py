dddf3d78c6b57c2ef5ca15dfc6a087021ceb6cea
Sergei Gordeev <gordeser@cvut.cz>
2023-02-14 21:09:41 +0100

    Change filter from incorrect to correct number

--- filters/IsCorrectNumber.py
+++ filters/IsCorrectNumber.py
@@ -2,10 +2,10 @@ from aiogram import types
 from aiogram.dispatcher.filters import Filter
 
 
-class IsIncorrectNumber(Filter):
+class IsCorrectNumber(Filter):
     async def check(self, message: types.Message):
         try:
             float(message.text)
-            return False
-        except ValueError:
             return True
+        except ValueError:
+            return False
