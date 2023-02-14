5a22ef512ef109bbb76786d9b4dd6acfe3d5b8a7
Sergei Gordeev <gordeser@cvut.cz>
2023-02-14 22:44:36 +0100

    Add function that add new page in notion database

--- notion.py
+++ notion.py
@@ -1,4 +1,7 @@
-from config import NOTION_API_TOKEN, DATABASE_ID
+from config import NOTION_API_TOKEN, CURRENCY_DATABASE_ID, EXPENSES_DATABASE_ID, CZK_PAGE_ID, RUB_PAGE_ID, EUR_PAGE_ID, \
+    USD_PAGE_ID, INCOMES_DATABASE_ID
+
+from datetime import datetime
 
 import requests
 import json
@@ -6,12 +9,12 @@ import json
 headers = {
     "Authorization": "Bearer " + NOTION_API_TOKEN,
     "Content-Type": "application/json",
-    "Notion-Version": "2022-02-22"
+    "Notion-Version": "2022-06-28"
 }
 
 
 def getAmountOfCurrencies():
-    readUrl = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+    readUrl = f"https://api.notion.com/v1/databases/{CURRENCY_DATABASE_ID}/query"
     res = requests.request("POST", readUrl, headers=headers)
     data = res.json()
     currencies = {
@@ -22,3 +25,58 @@ def getAmountOfCurrencies():
     }
     return currencies
 
+
+def addNewPage(database, name, currency, amount, comment):
+    readUrl = f"https://api.notion.com/v1/pages"
+
+    match currency:
+        case 'CZK':
+            curr = CZK_PAGE_ID
+        case 'RUB':
+            curr = RUB_PAGE_ID
+        case 'EUR':
+            curr = EUR_PAGE_ID
+        case 'USD':
+            curr = USD_PAGE_ID
+
+    match database:
+        case 'expense':
+            page = EXPENSES_DATABASE_ID
+        case 'income':
+            page = INCOMES_DATABASE_ID
+
+    data = json.dumps({
+        "parent": {
+            "database_id": page
+        },
+        "properties": {
+            "Name of item": {
+                "title": [{
+                    "text": {
+                        "content": name
+                    }
+                }]
+            },
+            "Amount": {
+                "number": amount
+            },
+            "Currency": {
+                "relation": [{
+                    "id": curr
+                }]
+            },
+            "Date": {
+                "date": {
+                    "start": datetime.today().isoformat() + '+01:00'
+                }
+            },
+            "Comment": {
+                "rich_text": [{
+                    "text": {
+                        "content": comment
+                    }
+                }]
+            }
+        }
+    })
+    requests.request("POST", readUrl, headers=headers, data=data)
